06a126fa9e051461f4f1b5434ffa1a6ceeb973aa
VigneswarM <imvigneswar@gmail.com>
2019-06-23 18:15:29 -0400

    Project Codec Correlation - changes - Mutation

--- Metrics Calculations/Project 2 - Apache Commons Codec/Correlation/version_codec_1.10/Codec version_1.10.py
+++ Metrics Calculations/Project 2 - Apache Commons Codec/Correlation/version_codec_1.10/Codec version_1.10.py
@@ -5,53 +5,56 @@
 
 
 import pandas as pd
-
-
-# In[2]:
-
+import scipy.stats  as stats
 
 df_collection = pd.read_csv('jacoco.csv', error_bad_lines=False)
 
 import matplotlib.pyplot as plt
 
-df_collection.head()
+
+# In[2]:
+
+
+df_collection['StatementCoverage'] =  (df_collection['LINE_COVERED'] / (df_collection['LINE_COVERED'] + df_collection['LINE_MISSED'])) * 100
+
+df_collection['BranchCoverage'] =  (df_collection['BRANCH_COVERED'] / (df_collection['BRANCH_COVERED'] + df_collection['BRANCH_MISSED'])) * 100
+
+df_collection['CodeComplexity'] =  df_collection['COMPLEXITY_COVERED'] + df_collection['COMPLEXITY_MISSED'];
+
+df_collection.plot(x='CodeComplexity', y='StatementCoverage', style='o')
 
 
 # In[3]:
 
 
-df_collection.plot(x='Code Complexity', y='Statement Coverage', style='o')
+df_collection.plot(x='CodeComplexity', y='BranchCoverage', style='o')
+
+plt.scatter(df_collection['CodeComplexity'], df_collection['StatementCoverage'])
+plt.show()
 
 
 # In[4]:
 
 
-df_collection.plot(x='Code Complexity', y='Branch Coverage', style='o')
+df_collection.corr(method ='spearman')
+
+df_collection[['CodeComplexity','StatementCoverage']].corr(method ='spearman')
+
+df_clean = df_collection.dropna()
+stats.spearmanr(df_clean['StatementCoverage'], df_clean['CodeComplexity'])
+
+df_collection[['CodeComplexity','BranchCoverage']].corr(method ='spearman')
 
 
 # In[5]:
 
 
-plt.scatter(df_collection['Code Complexity'], df_collection['Statement Coverage'])
-plt.show()
+df_clean = df_collection.dropna()
+stats.spearmanr(df_clean['BranchCoverage'], df_clean['CodeComplexity'])
 
+print('Statment Coverage '+str((df_collection.LINE_COVERED.sum()/(df_collection.LINE_MISSED.sum() + df_collection.LINE_COVERED.sum()))*100))
 
-# In[6]:
-
-
-df_collection.corr(method ='spearman')
-
-
-# In[7]:
-
-
-df_collection[['Code Complexity','Statement Coverage']].corr(method ='spearman')
-
-
-# In[8]:
-
-
-df_collection[['Code Complexity','Branch Coverage']].corr(method ='spearman')
+print('Branch Coverage '+str((df_collection.BRANCH_COVERED.sum()/(df_collection.BRANCH_MISSED.sum() + df_collection.BRANCH_COVERED.sum()))*100))
 
 
 # In[ ]:
--- Metrics Calculations/Project 2 - Apache Commons Codec/Correlation/version_codec_1.12/correlation between coverage && complexity/Project 2 - Apache Commons Codec.py
+++ Metrics Calculations/Project 2 - Apache Commons Codec/Correlation/version_codec_1.12/correlation between coverage && complexity/Project 2 - Apache Commons Codec.py
@@ -5,61 +5,56 @@
 
 
 import pandas as pd
+import scipy.stats  as stats
+
+df_collection = pd.read_csv('jacoco.csv', error_bad_lines=False)
+
+import matplotlib.pyplot as plt
 
 
 # In[2]:
 
 
-df_collection = pd.read_csv('jacoco.csv', error_bad_lines=False)
+df_collection['StatementCoverage'] =  (df_collection['LINE_COVERED'] / (df_collection['LINE_COVERED'] + df_collection['LINE_MISSED'])) * 100
+
+df_collection['BranchCoverage'] =  (df_collection['BRANCH_COVERED'] / (df_collection['BRANCH_COVERED'] + df_collection['BRANCH_MISSED'])) * 100
+
+df_collection['CodeComplexity'] =  df_collection['COMPLEXITY_COVERED'] + df_collection['COMPLEXITY_MISSED'];
+
+df_collection.plot(x='CodeComplexity', y='StatementCoverage', style='o')
 
 
 # In[3]:
 
 
-import matplotlib.pyplot as plt
+df_collection.plot(x='CodeComplexity', y='BranchCoverage', style='o')
+
+plt.scatter(df_collection['CodeComplexity'], df_collection['StatementCoverage'])
+plt.show()
 
 
 # In[4]:
 
 
-df_collection.head()
+df_collection.corr(method ='spearman')
+
+df_collection[['CodeComplexity','StatementCoverage']].corr(method ='spearman')
+
+df_clean = df_collection.dropna()
+stats.spearmanr(df_clean['StatementCoverage'], df_clean['CodeComplexity'])
+
+df_collection[['CodeComplexity','BranchCoverage']].corr(method ='spearman')
 
 
 # In[5]:
 
 
-df_collection.plot(x='Code Complexity', y='Statement Coverage', style='o')
+df_clean = df_collection.dropna()
+stats.spearmanr(df_clean['BranchCoverage'], df_clean['CodeComplexity'])
 
+print('Statment Coverage '+str((df_collection.LINE_COVERED.sum()/(df_collection.LINE_MISSED.sum() + df_collection.LINE_COVERED.sum()))*100))
 
-# In[6]:
-
-
-df_collection.plot(x='Code Complexity', y='Branch Coverage', style='o')
-
-
-# In[7]:
-
-
-plt.scatter(df_collection['Code Complexity'], df_collection['Statement Coverage'])
-plt.show()
-
-
-# In[8]:
-
-
-df_collection.corr(method ='spearman')
-
-
-# In[9]:
-
-
-df_collection[['Code Complexity','Statement Coverage']].corr(method ='spearman')
-
-
-# In[10]:
-
-
-df_collection[['Code Complexity','Branch Coverage']].corr(method ='spearman')
+print('Branch Coverage '+str((df_collection.BRANCH_COVERED.sum()/(df_collection.BRANCH_MISSED.sum() + df_collection.BRANCH_COVERED.sum()))*100))
 
 
 # In[ ]:
