1b035169600665b12bb455d72aa8f75400006477
VigneswarM <imvigneswar@gmail.com>
2019-06-23 13:13:55 -0400

    Correlation Project 5

---
+++ Metrics Calculations/Project 5 - Apache Commons Pool/Correlation/version_2.6.1/Pool 2.6.1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+df_collection = pd.read_csv('jacoco_2.6.1.csv', error_bad_lines=False)
+
+import matplotlib.pyplot as plt
+
+df_collection.head()
+
+
+# In[2]:
+
+
+df_collection.plot(x='Code Complexity', y='Statement Coverage', style='o')
+
+df_collection.plot(x='Code Complexity', y='Branch Coverage', style='o')
+
+plt.scatter(df_collection['Code Complexity'], df_collection['Statement Coverage'])
+plt.show()
+
+
+# In[3]:
+
+
+plt.scatter(df_collection['Code Complexity'], df_collection['Statement Coverage'])
+plt.show()
+
+
+# In[4]:
+
+
+df_collection.corr(method ='spearman')
+
+df_collection[['Code Complexity','Statement Coverage']].corr(method ='spearman')
+
+df_collection[['Code Complexity','Branch Coverage']].corr(method ='spearman')
+
+
+# In[ ]:
+
+
+
+
